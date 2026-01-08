cc3d844f9f10a34510292818dbab697b3dcd9ff0
Christian Alexandre <christian.alexandre-ext@viavarejo.com.br>
2026-01-08 11:14:57 -0300

    chore: decrease iOS target to 13.0+.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AppNetworkMonitor",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v13),
     ],
     products: [
         .library(
